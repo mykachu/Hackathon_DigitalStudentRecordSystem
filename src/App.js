b9c2132726dffb7d2409177d6be3d8703ffa41ee
Jerry <nguyentruongdat1661999@gmail.com>
2020-12-26 17:30:36 +0700

    add StudentPage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import logo from './logo.svg';
-// import './App.css';
-import LoginPage from './components/LoginPage';
+// import logo from './logo.svg';
+import './App.css';
+// import LoginPage from './components/LoginPage';
+import StudentPage from './components/StudentPage';
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Say meow meow.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StudentPage />
     </div>
   );
 }
