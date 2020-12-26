1a68136ed891a2f3996815353c517363faf65f9e
Jerry <nguyentruongdat1661999@gmail.com>
2020-12-26 22:37:33 +0700

    create something new

---
+++ src/components/StudentPage/AboutPage/AboutPage.js
@@ -0,0 +1,12 @@
+import 'antd/dist/antd.css';
+import React from 'react';
+
+class AboutPage extends React.Component {
+    render(){
+        return (
+            <p> This is AboutPage</p>
+        )
+    }
+}
+
+export default AboutPage;
