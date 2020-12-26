31eb9fd1be67dc6974e2aa759fbbd0da2d4c185b
MiQan <53591126+miqann@users.noreply.github.com>
2020-12-26 19:25:15 +0700

    modify StudentPage

--- src/components/StudentPage.js
+++ src/components/StudentPage.js
@@ -2,19 +2,22 @@ import './StudentPage.css';
 import 'antd/dist/antd.css';
 import { Layout, Menu, Breadcrumb } from 'antd';
 import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import { Switch } from 'antd';
 
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
 
 function StudentPage() {
     return (
+
         <Layout>
             <Header className="header">
                 <div className="logo" />
                 <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-                    <Menu.Item key="1">nav 1</Menu.Item>
-                    <Menu.Item key="2">nav 2</Menu.Item>
-                    <Menu.Item key="3">nav 3</Menu.Item>
+                    <Menu.Item key="1">Tiểu học</Menu.Item>
+                    <Menu.Item key="2">Trung học cơ sở</Menu.Item>
+                    <Menu.Item key="3">Trung học phổ thông</Menu.Item>
+                    <Menu.Item key="4">Đại học</Menu.Item>
                 </Menu>
             </Header>
             <Layout>
@@ -25,21 +28,20 @@ function StudentPage() {
                         defaultOpenKeys={['sub1']}
                         style={{ height: '100%', borderRight: 0 }}
                     >
-                        <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-                            <Menu.Item key="1">option1</Menu.Item>
-                            <Menu.Item key="2">option2</Menu.Item>
-                            <Menu.Item key="3">option3</Menu.Item>
-                            <Menu.Item key="4">option4</Menu.Item>
+                        <SubMenu key="sub1" icon={<UserOutlined />} title="Học bạ">
+                            <Menu.Item key="1">Cả năm</Menu.Item>
+                            <Menu.Item key="2">Hạnh kiểm</Menu.Item>
+
                         </SubMenu>
-                        <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+                        <SubMenu key="sub2" icon={<LaptopOutlined />} title="Lấy link">
                             <Menu.Item key="5">option5</Menu.Item>
                             <Menu.Item key="6">option6</Menu.Item>
                             <Menu.Item key="7">option7</Menu.Item>
                             <Menu.Item key="8">option8</Menu.Item>
                         </SubMenu>
-                        <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
-                            <Menu.Item key="9">option9</Menu.Item>
-                            <Menu.Item key="10">option10</Menu.Item>
+                        <SubMenu key="sub3" icon={<NotificationOutlined />} title="Tuỳ chỉnh chia sẻ">
+                            <Menu.Item key="9"><Switch></Switch> Trường học</Menu.Item>
+                            <Menu.Item key="10"></Menu.Item>
                             <Menu.Item key="11">option11</Menu.Item>
                             <Menu.Item key="12">option12</Menu.Item>
                         </SubMenu>
@@ -47,9 +49,9 @@ function StudentPage() {
                 </Sider>
                 <Layout style={{ padding: '0 24px 24px' }}>
                     <Breadcrumb style={{ margin: '16px 0' }}>
-                        <Breadcrumb.Item>Home</Breadcrumb.Item>
-                        <Breadcrumb.Item>List</Breadcrumb.Item>
-                        <Breadcrumb.Item>App</Breadcrumb.Item>
+                        <Breadcrumb.Item>Trang chủ</Breadcrumb.Item>
+                        <Breadcrumb.Item>Xem điểm</Breadcrumb.Item>
+                        {/* <Breadcrumb.Item>App</Breadcrumb.Item> */}
                     </Breadcrumb>
                     <Content
                         className="site-layout-background"
@@ -58,7 +60,7 @@ function StudentPage() {
                             margin: 0,
                             minHeight: 280,
                         }}
-                    >
+                    ><Switch></Switch>
                         Content
           </Content>
                 </Layout>
